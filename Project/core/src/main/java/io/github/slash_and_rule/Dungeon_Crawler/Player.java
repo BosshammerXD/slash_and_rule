16c69ef5217edac7d598cb799b591bcff7e612e5
jumbshd <willibatenwb@gmail.com>
2025-06-08 19:43:32 +0200

    Revert Test Changes
    Signed-off-by: jumbshd <willibatenwb@gmail.com>

--- Project/core/src/main/java/io/github/slash_and_rule/Dungeon_Crawler/Player.java
+++ Project/core/src/main/java/io/github/slash_and_rule/Dungeon_Crawler/Player.java
@@ -54,7 +54,7 @@ public class Player extends BasePhysicsObject implements Displayable, Updatetabl
 
         Vector2 movDir = new Vector2(0, 0);
 
-        if (Gdx.input.isKeyPressed(Keys.D)) {
+        if (Gdx.input.isKeyPressed(Keys.W)) {
             movDir.y += 1;
             // hello
         }

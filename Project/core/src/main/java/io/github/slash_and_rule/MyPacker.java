67327464d7d29d725b815c6c9dac91cbe98ed14a
jumbshd <willibatenwb@gmail.com>
2025-07-14 00:32:03 +0200

    Updated Player Texture Atlas and Packing Configuration

--- Project/core/src/main/java/io/github/slash_and_rule/MyPacker.java
+++ Project/core/src/main/java/io/github/slash_and_rule/MyPacker.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class MyPacker {
     public static void main(String[] args) {
-        TexturePacker.process("Project/assets/weapons/RawBasicSword", "Project/assets/weapons/BasicSword",
-                "BasicSword");
+        TexturePacker.process("Project/assets/entities/PlayerRaw", "Project/assets/entities/Player",
+                "Player");
     }
 }
